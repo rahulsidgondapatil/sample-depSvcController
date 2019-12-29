f3dc0d5c733677de8e0582503a56629fde67f167
rahulsidgondapatil <rahulsidgondapatil@gmail.com>
2019-12-29 17:34:17 +0530

    Generated code files

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -27,7 +27,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   pkg/generated \
-  github.com/rahulsidgondapatil/sample-depSvcController/pkg/apis \
+  github.com/infracloudio/sample-depSvcController/pkg/apis \
   SampleDepSvcController:v1alpha1 \
   --output-base "${SCRIPT_ROOT}" \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
